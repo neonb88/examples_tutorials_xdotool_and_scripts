1600955d9fb614311529e7045a2a1449f870fa03
neonb88 <nathanbendich@gmail.com>
2019-09-09 02:34:17 -0400

    click.sh :   test whether Available commands:   getactivewindow   getwindowfocus   getwindowname   getwindowpid   getwindowgeometry   getdisplaygeometry   search   selectwindow   help   version   behave   behave_screen_edge   click   getmouselocation   key   keydown   keyup   mousedown   mousemove   mousemove_relative   mouseup   set_window   type   windowactivate   windowfocus   windowkill   windowclose   windowmap   windowminimize   windowmove   windowraise   windowreparent   windowsize   windowunmap   set_num_desktops   get_num_desktops   set_desktop   get_desktop   set_desktop_for_window   get_desktop_for_window   get_desktop_viewport   set_desktop_viewport   exec   sleep commands even work inside of a bash shell script

---
+++ click.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+#   xdotool teaching!   https://www.semicomplete.com/projects/xdotool/xdotool.xhtml
+
+xdotool mousedown 1
+
+
+
+#   Examples:
+#     xdotool mousemove 163 757
+#     xdotool getmouselocation
+#     xdotool key Return
+#     xdotool mousedown 1 # main (left) mouse "click" button
